8f8a8ac415ff5dae88a921812e4b0999ae223de2
Nshstephan <nshstephan@gmail.com>
2021-02-19 11:00:43 +0400

    DotEnv HomeWork

---
+++ DotEnv_project/main.js
@@ -0,0 +1,13 @@
+require('./dotenv').config();
+
+
+console.log(process.env.PORT);
+console.log(process.env.NODE_ENV);
+console.log(process.env.P);
+console.log(process.env.EMAIL);
+console.log(process.env.Ping);
+console.log(process.env.PORT);
+console.log(process.env);
+
+
+
