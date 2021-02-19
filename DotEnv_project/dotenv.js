285ee69895d61bf82a7390bdd3ef531caceab7f6
Nshstephan <nshstephan@gmail.com>
2021-02-19 11:08:05 +0400

    Update dotenv.js

--- DotEnv_project/dotenv.js
+++ DotEnv_project/dotenv.js
@@ -1,20 +1,24 @@
 const fs = require('fs');
 
 function config() {
-    let data = fs.readFileSync('./.env', 'utf8');
-    let byLine = data.split('\n');
-    for (let i = 0; i < byLine.length; ++i) {
-        let splitted = byLine[i].split('=');
-        let key = splitted[0];
-        let value;
-        if (splitted.length >= 3) {
-            value = splitted.slice(1).join('=');
-        } else {
-            value = splitted[1];
+    try {
+        let data = fs.readFileSync('./.env', 'utf8');
+        let byLine = data.split('\n');
+        for (let i = 0; i < byLine.length; ++i) {
+            let splitted = byLine[i].split('=');
+            let key = splitted[0];
+            let value;
+            if (splitted.length >= 3) {
+                value = splitted.slice(1).join('=');
+            } else {
+                value = splitted[1];
+            }
+            if (!key.startsWith("#") && key !== '') {
+                process.env[key] = value;
+            }
         }
-        if (!key.startsWith("#") && key !== '') {
-            process.env[key] = value;
-        }
+    } catch (err) {
+        throw new Error("Something wen wrong.");
     }
 }
 
